9afd744297f6c1d2334157d56e3de87d6b11f81f
ireshdilsh <ireshdilshan46@gmail.com>
2024-12-17 22:49:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from './pages/Home'
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/Header.css"
+import logo from "../assets/logo-original.png"
+export default function Header() {
+  return (
+    <div>
+       
+    </div>
+  )
+}
