92bfcb0a1dba934369f4661ef13780449863a934
ireshdilsh <ireshdilshan46@gmail.com>
2024-12-18 10:01:39 +0530

    complete navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,21 @@ import logo from "../assets/logo-original.png"
 export default function Header() {
   return (
     <div>
-       
+       <nav>
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="nav-items">
+          <a href="">Home</a>
+          <a href="">Prices</a>
+          <a href="">Models</a>
+          <a href="">About</a>
+          <a href="">Contact</a>
+        </div>
+        <div className="button">
+        <button type="button" class="btn btn-primary">Get started</button>
+        </div>
+       </nav>
     </div>
   )
 }
